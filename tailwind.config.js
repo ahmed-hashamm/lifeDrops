3f831551d2f564198e39c93f6a8d426909040d67
Hasham Ahmed <ahmedhasham90012@gmail.com>
2024-05-09 23:55:36 +0500

    fixed tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
